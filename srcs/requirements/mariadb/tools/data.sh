8d2331d1397e85dc9fbc2068983a3447ecd720a5
Yousra <110408013+yousrachakib@users.noreply.github.com>
2024-03-13 12:36:15 +0000

    mariadb and wordpress done

--- srcs/requirements/mariadb/tools/data.sh
+++ srcs/requirements/mariadb/tools/data.sh
@@ -1,37 +1,33 @@
 #!/bin/bash
 
-DB_NAME=yodata
+DB_NAME=Wordpress
 DB_USER=yochakib
-DB_PSSWRD=123
+DB_PSSWRD=1234
+MYSQL_ROOT_PASSWORD=1234
 
-service mysql start 
+service mysql start
 #this ensures that the database server is running and ready to accept connections.
 
-mysql -e "CREATE DATABASE IF NOT EXISTS '$DB_NAME'";
-# This line uses the mysql command-line tool to connect to the MySQL/MariaDB server as the root user (-u root) and prompts for the root user's password (-p).
-# It then executes an SQL statement to create a database with the name specified in the $DB_NAME variable.
-# The IF NOT EXISTS clause ensures that the database is created only if it doesn't already exist.
+mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME";
 
+# This line uses the mysql command-line tool to connect to the MySQL/MariaDB server as t
 mysql -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PSSWRD'";
 # This line creates a user with the name specified in the $DB_USER variable and sets the user's password to the value in the $DB_PSSWRD variable. 
 # The IF NOT EXISTS clause ensures that the user is created only if it doesn't already exist. The @'%' part specifies that the user can connect from any host.
 
 mysql -e "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%';" ;
-# This line grants all privileges to the user specified in the $DB_USER variable. 
-# The *.* syntax indicates that the user has full privileges on all databases and tables. The @'%' part specifies that the user can connect from any host.
 
 mysql -e "FLUSH PRIVILEGES;"
-# This line flushes the privileges to ensure that the changes made in the previous GRANT statement take effect immediately.
 
-#mysql -e "alter user 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD';" ;
-#This line changes the password for the root user of the MySQL/MariaDB server. The new password is specified in the $MYSQL_ROOT_PASSWORD variable.
+
+mysql -e "alter user 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD';" ;
+
 
 mysql -e "FLUSH PRIVILEGES;"
-# This line flushes the privileges to ensure that the changes made in the previous GRANT statement take effect immediately.
 
-#kill `cat /var/run/mysqld/mysqld.pid`
-#This line kills the MySQL/MariaDB server process by sending a signal to the process ID (PID) specified in the /var/run/mysqld/mysqld.pid file. 
-#Killing the process will stop the MySQL/MariaDB server.
+
+#mysql -e "SHUTDOWN;"
+kill `cat /var/run/mysqld/mysqld.pid`
 
 mysqld
-#this line start the server again
+#this line start the server again/
--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -1,13 +1,44 @@
-#!/bin/sh
+#!/bin/bash
+
+MYSQL_DATABASE=Wordpress
+MYSQL_ROOT_USER=yochakib
+MYSQL_ROOT_PASSWORD=1234
+HOST=mariadb
+
+
+
+DOMAIN_NAME=yochakib
+MYSQL_USER=elliech
+MYSQL_PASSWORD=666
+
+
 
 #Downloads the WordPress core files using the wp-cli
+
+
+mkdir -p /run/php/;
+
+touch /run/php/php7.3-fpm.pid; #Store PID files for PHP processes managed by the PHP-FPM
+
+sed -i "s/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/" "/etc/php/7.3/fpm/pool.d/www.conf"
+
+mkdir -p var/www/html;
+
+cd var/www/html
+
 wp core download  --allow-root
 
-#Creates the WordPress configuration file wp-config.php
-wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_ROOT_USER --dbpass=$MYSQL_ROOT_PASSWORD --dbhost=mariadb --allow-root
+cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php;
+
+sed -i "s/database_name_here/${MYSQL_DATABASE}/g" "/var/www/html/wp-config.php"
+sed -i "s/username_here/${MYSQL_ROOT_USER}/g" "/var/www/html/wp-config.php"
+sed -i "s/password_here/${MYSQL_ROOT_PASSWORD}/g" "/var/www/html/wp-config.php"
+sed -i "s/localhost/${HOST}/g" "/var/www/html/wp-config.php"
 
 #Installs WordPress with the provided URL
-wp core install --url=$DOMAIN_NAME --title=$DOMAIN_NAME --admin_user=$MYSQL_ROOT_USER --admin_password=$MYSQL_ROOT_PASSWORD --admin_email=amgharmahdi1@gmail.com --allow-root
+wp core install --url=$DOMAIN_NAME --title=$DOMAIN_NAME --admin_user=$MYSQL_ROOT_USER --admin_password=$MYSQL_ROOT_PASSWORD --admin_email=yousra.chkib@icloud.com --allow-root
 
 #Creates a new WordPress user
 wp user create $MYSQL_USER randomMail@gmail.com --role=author --user_pass=$MYSQL_PASSWORD  --allow-root
+
+exec "$@"
