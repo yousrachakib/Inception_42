08a2085fd38e613c57b01280065b945d45db1557
Yousra <110408013+yousrachakib@users.noreply.github.com>
2024-03-23 14:09:38 +0000

    mandatory done

--- srcs/requirements/mariadb/tools/data.sh
+++ srcs/requirements/mariadb/tools/data.sh
@@ -1,59 +1,18 @@
 #!/bin/bash
-
-# DB_NAME=Wordpress
-# DB_USER=yochakib
-# DB_PSSWRD=1234
-# MYSQL_ROOT_PASSWORD=1234
-
-# service mysql start
-# #this ensures that the database server is running and ready to accept connections.
-
-# mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME";
-
-# # This line uses the mysql command-line tool to connect to the MySQL/MariaDB server as t
-# mysql -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PSSWRD'";
-# # The @'%' part specifies that the user can connect from any host.
-
-# mysql -e "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%';" ;
-# #*.* all databases and tables in the MySQL server.
-
-# mysql -e "FLUSH PRIVILEGES;"
-
-# mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$MYSQL_ROOT_PASSWORD';"
-
-# mysql -e "FLUSH PRIVILEGES;"
-
-# mysql -u root -p "$MYSQL_ROOT_PASSWORD"
-
-# #mysql -e "SHUTDOWN;"
-# kill `cat /var/run/mysqld/mysqld.pid`
-
-# mysqld
-# #this line start the server again
-#------------------------------------------------------------------------
-#!/bin/bash
-
-DB_NAME=wp_data
-DB_USER=yochakib
-DB_PSSWRD=1234
-MYSQL_ROOT_PASSWORD=1234
-
 service mysql start
 
-mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;";
+mysql -e"CREATE DATABASE IF NOT EXISTS $DB_NAME;";
 
-mysql -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PSSWRD';";
+mysql -e"CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PSSWRD';";
 
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%';"; # replace *.* by database name
+mysql -e"GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%';"; # replace *.* by database name
 
-mysql -e "FLUSH PRIVILEGES;";
+mysql -e"FLUSH PRIVILEGES;";
 
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';";
+mysql -e"ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';";
 
-mysql -e "FLUSH PRIVILEGES;";
+mysql -e"FLUSH PRIVILEGES;";
 
-#mysql -u root -p"$MYSQL_ROOT_PASSWORD"
-
-kill `cat /var/run/mysqld/mysqld.pid`
+kill `cat /var/run/mysqld/mysqld.pid` 
 
 exec "$@"
--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -1,44 +1,29 @@
 #!/bin/bash
-MYSQL_DATABASE=wp_data
-MYSQL_ROOT_USER=yochakib
-MYSQL_ROOT_PASSWORD=1234
-HOST=mariadb
 
-DOMAIN_NAME=yochakib
-MYSQL_USER=elliech
-MYSQL_PASSWORD=666
+mkdir -p /run/php/;
+    touch /run/php/php7.3-fpm.pid; #Store PID files for PHP processes managed by the PHP-FPM
+    sed -i "s/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/" "/etc/php/7.3/fpm/pool.d/www.conf"
 
 if [ ! -f /var/www/html/wp-config.php ]; then
 
-mkdir -p /run/php/;
+	wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar;
+	chmod +x wp-cli.phar
+	mv wp-cli.phar /usr/local/bin/wp
 
-touch /run/php/php7.3-fpm.pid; #Store PID files for PHP processes managed by the PHP-FPM
+	mkdir -p var/www/html
+	cd var/www/html
+	wp core download  --allow-root
 
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+	cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php;
+	sed -i "s/database_name_here/${MYSQL_DATABASE}/g" "/var/www/html/wp-config.php"
+	sed -i "s/username_here/${MYSQL_ROOT_USER}/g" "/var/www/html/wp-config.php"
+	sed -i "s/password_here/${MYSQL_ROOT_PASSWORD}/g" "/var/www/html/wp-config.php"
+	sed -i "s/localhost/${HOST}/g" "/var/www/html/wp-config.php"
 
-chmod +x wp-cli.phar
-
-mv wp-cli.phar /usr/local/bin/wp
-
-sed -i "s/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/" "/etc/php/7.3/fpm/pool.d/www.conf"
-
-mkdir -p var/www/html
-
-cd var/www/html
-
-wp core download  --allow-root
-
-cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php;
-
-sed -i "s/database_name_here/${MYSQL_DATABASE}/g" "/var/www/html/wp-config.php"
-sed -i "s/username_here/${MYSQL_ROOT_USER}/g" "/var/www/html/wp-config.php"
-sed -i "s/password_here/${MYSQL_ROOT_PASSWORD}/g" "/var/www/html/wp-config.php"
-sed -i "s/localhost/${HOST}/g" "/var/www/html/wp-config.php"
-
-#Installs WordPress with the provided URL
-wp core install --url=$DOMAIN_NAME --title=$DOMAIN_NAME --admin_user=$MYSQL_ROOT_USER --admin_password=$MYSQL_ROOT_PASSWORD --admin_email=yousra.chkib@icloud.com --allow-root
-#Creates a new WordPress use
-wp user create $MYSQL_USER randomMail@gmail.com --role=author --user_pass=$MYSQL_PASSWORD  --allow-root
-echo "Installed succefully!"
+	#Installs WordPress with the provided URL
+	wp core install --url=$DOMAIN_NAME --title=$DOMAIN_NAME --admin_user=$MYSQL_ROOT_USER --admin_password=$MYSQL_ROOT_PASSWORD --admin_email=yousra.chkib@icloud.com --allow-root
+	#Creates a new WordPress use
+	wp user create $MYSQL_USER randomMail@gmail.com --role=author --user_pass=$MYSQL_PASSWORD  --allow-root
+	echo "Installed succefully!"
 fi
 exec "$@"
