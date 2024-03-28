b3f257b09a0905326728887de939062f48946fcf
Yousra <110408013+yousrachakib@users.noreply.github.com>
2024-03-28 16:01:11 +0000

    done for this project

---
+++ Makefile
@@ -0,0 +1,28 @@
+# Define variables
+COMPOSE_FILE := ./srcs/docker-compose.yml
+WORDPRESS_DATA_DIR := ./srcs/data/wordpress
+MARIA_DB_DATA_DIR := ./srcs/data/mariadb
+
+# Define targets
+.PHONY: up down logs clean build
+
+up: build
+	docker-compose -f $(COMPOSE_FILE) up -d 
+
+down:
+	docker-compose -f $(COMPOSE_FILE) down
+
+logs:
+	docker-compose -f $(COMPOSE_FILE) logs -f
+
+clean:
+	docker-compose -f $(COMPOSE_FILE) down --volumes --remove-orphans
+	cd $(WORDPRESS_DATA_DIR) && rm -rf *
+	cd $(MARIA_DB_DATA_DIR) && rm -rf *
+
+build: create_dirs
+	docker-compose -f $(COMPOSE_FILE) build
+
+create_dirs:
+	mkdir -p $(WORDPRESS_DATA_DIR)
+	mkdir -p $(MARIA_DB_DATA_DIR)
--- srcs/requirements/mariadb/tools/data.sh
+++ srcs/requirements/mariadb/tools/data.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
+
 service mysql start
+sleep 1
 
 mysql -e"CREATE DATABASE IF NOT EXISTS $DB_NAME;";
 
 mysql -e"CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PSSWRD';";
 
-mysql -e"GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%';"; # replace *.* by database name
+mysql -e"GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';";
 
 mysql -e"FLUSH PRIVILEGES;";
 
-mysql -e"ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';";
+mysql -e "UPDATE mysql.user SET authentication_string = PASSWORD('${MYSQL_ROOT_PASSWORD}') WHERE User = 'root' AND Host = 'localhost';"
 
 mysql -e"FLUSH PRIVILEGES;";
+
 
 kill `cat /var/run/mysqld/mysqld.pid` 
 
